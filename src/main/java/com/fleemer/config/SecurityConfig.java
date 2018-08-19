0f4ae1f455d698e02c9f9ab97b4d64512293861a
Oleg Shishkin <olegshishkin@gmail.com>
2018-08-19 13:57:20 +0300

    security rules optimizing

--- src/main/java/com/fleemer/config/SecurityConfig.java
+++ src/main/java/com/fleemer/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,19 +29,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/static/**", "/user/create");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests()
-                    .antMatchers("/user/create", "/static/**").permitAll()//todo чтобы не обрабатывал эти url
-                    .anyRequest().authenticated()
-                .and()
                 .formLogin()
-                    .defaultSuccessUrl("/", true)
                     .loginPage("/login")
+                    .defaultSuccessUrl("/", true)
                     .permitAll()
                 .and()
                 .logout()
-                    .permitAll();
+                    .permitAll()
+                .and()
+                .authorizeRequests()
+                    .anyRequest()
+                    .authenticated();
     }
 
     @Bean
