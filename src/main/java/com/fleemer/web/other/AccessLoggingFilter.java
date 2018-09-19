3e6d928162ebbea910c5f9f199d8fe3d0f257527
Oleg Shishkin <olegshishkin@gmail.com>
2018-09-20 01:48:39 +0300

    fix access logging

--- src/main/java/com/fleemer/web/other/AccessLoggingFilter.java
+++ src/main/java/com/fleemer/web/other/AccessLoggingFilter.java
@@ -1,18 +1,17 @@
 package com.fleemer.web.other;
 
-import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.access.spi.IAccessEvent;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
 
-public class AccessLoggingFilter extends Filter<ILoggingEvent> {
+public class AccessLoggingFilter extends Filter<IAccessEvent> {
     private static final String WEBSOCKET_HANDSHAKE_URL = "/chat/handshake";
 
     @Override
-    public FilterReply decide(ILoggingEvent event) {
-        if (event.getMessage().startsWith(WEBSOCKET_HANDSHAKE_URL)) {
+    public FilterReply decide(IAccessEvent event) {
+        if (event.getRequestURI().startsWith(WEBSOCKET_HANDSHAKE_URL)) {
             return FilterReply.DENY;
-        } else {
-            return FilterReply.ACCEPT;
         }
+        return FilterReply.ACCEPT;
     }
 }
